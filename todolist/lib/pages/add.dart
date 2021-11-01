a6b6e0bcb4ce9102da5aba4ead60e5e07e29a01d
michaelfu1 <91065687+michaelfu1@users.noreply.github.com>
2021-11-02 01:11:50 +0700

    App flutter

---
+++ todolist/lib/pages/add.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+// http method package
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+class AddPage extends StatefulWidget {
+  const AddPage({Key? key}) : super(key: key);
+
+  @override
+  _AddPageState createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  TextEditingController todo_title = TextEditingController();
+  TextEditingController todo_detail = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('เพิ่มรายการใหม่'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            //ช่องกรอกข้อมูล title
+            TextField(
+                controller: todo_title,
+                decoration: InputDecoration(
+                    labelText: 'รายการที่ต้องทำ',
+                    border: OutlineInputBorder())),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+                minLines: 4,
+                maxLines: 8,
+                controller: todo_detail,
+                decoration: InputDecoration(
+                    labelText: 'รายละเอียด', border: OutlineInputBorder())),
+            SizedBox(
+              height: 30,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: ElevatedButton(
+                  onPressed: () {
+                    print('------------');
+                    print('title: ${todo_title.text}');
+                    print('detail: ${todo_detail.text}');
+                    postTodo();
+                    setState(() {
+                      todo_title.clear();
+                      todo_detail.clear();
+                    });
+                  },
+                  child: Text("เพิ่มรายการ"),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xFF388DFC)),
+                      padding: MaterialStateProperty.all(
+                          EdgeInsets.fromLTRB(50, 20, 50, 20)),
+                      textStyle:
+                          MaterialStateProperty.all(TextStyle(fontSize: 30)))),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future postTodo() async {
+    //var url = Uri.http('af2c-223-24-190-224.ngrok.io', '/api/post-todolist');
+    var url = Uri.http('192.168.1.164:8000', '/api/post-todolist');
+    Map<String, String> header = {"Content-type": "application/json"};
+    String jsondata =
+        //'{"title":"เขียน App flutter", "detail":"เรียนเขียนแอพทุกวันเสาร์"}';
+        '{"title":"${todo_title.text}", "detail":"${todo_detail.text}"}';
+    var response = await http.post(url, headers: header, body: jsondata);
+    print('------result-----');
+    print(response.body);
+  }
+}
